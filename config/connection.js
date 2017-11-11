c325120d237b6a7db2cba5557c9d10502499884f
vidman22 <jvidmar37@gmail.com>
2017-11-10 23:27:52 -0800

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{ mysql.createConnection({
 	port: 3306,
 	host: "localhost",
 	user: "root",
 	password: "",
 	database: "burgers_db"
-});
+   });
+};
 
 connection.connect(function(err) {
 	if (err) {
